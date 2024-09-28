b71006fa85c1dc37b1e94e75e846c014ba9868cb
DaTo125 <mikeladzedato73@gmail.com>
2024-09-28 22:08:43 +0300

    Add files via upload

---
+++ level 4/level 4/claswork.py
@@ -0,0 +1,45 @@
+#davaleba 1
+num1=1
+num2=2
+sum=(num1+num2)
+print(sum)
+
+#davaleba 2
+price=50
+price=50
+print(price)
+
+
+
+#Debugging არის პროგრამირებაში შეცდომების აღმოჩენის და გასწორების პროცესი
+
+#1)
+print("Hello, World!")
+
+#2)
+greeting = "Good Morning"
+print(greeting)
+
+#3)
+first_name = "Alice"
+last_name = "Smith"
+print( first_name + last_name)
+
+#4)
+number = 50
+print("The number is: " + str(number))
+
+#5)
+name = "George"
+print("hello" + name)
+
+#6)
+age = 25
+print("I am" + str(age) + "years old")
+
+
+#7)
+num1 = 10
+num2 = 20
+result = num1 + num2
+print(result)
